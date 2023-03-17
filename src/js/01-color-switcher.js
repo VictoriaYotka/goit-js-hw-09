b422b92a93167885e5af9e4c0d8d65b3562b74a1
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-03-17 16:13:40 +0200

    task-1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const bodyEl = document.body;
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
 
+startBtnEl.addEventListener('click', colorSwitchHandler);
+stopBtnEl.addEventListener('click', stopColorSwitchHandler);
+
+function colorSwitchHandler() {
+  startBtnEl.disabled = true;
+  stopBtnEl.disabled = false;
+
+  switchInterval = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopColorSwitchHandler() {
+  startBtnEl.disabled = false;
+  stopBtnEl.disabled = true;
+
+  clearInterval(switchInterval);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
