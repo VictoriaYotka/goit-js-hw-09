20daf8ad886f278233c0fba748d22120ab19e015
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-03-24 08:49:41 +0200

    task-3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,50 @@
+const formEl = document.querySelector('.form');
+
+const promiseObj = {};
+
+let timeoutId = null;
+
+formEl.addEventListener('submit', formSubmitHandler);
+
+function formSubmitHandler (event) {
+  event.preventDefault();
+
+  formValuesPicker();
+
+  let { delay, step, amount } = promiseObj;
+
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);      
+      }) 
+
+      delay += step;
+
+      event.currentTarget.reset()
+  }     
+}
+
+function formValuesPicker () {
+  promiseObj.delay = Number(event.currentTarget.elements.delay.value);
+  promiseObj.step = Number(event.currentTarget.elements.step.value);
+  promiseObj.amount = Number(event.currentTarget.elements.amount.value);
+
+  return promiseObj;
+}
+
 function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  timeoutId = setTimeout(() => {
+      if (shouldResolve) {
+      resolve ({ position, delay });
+      } else {
+      reject ({ position, delay });
+      }}, delay)        
+  }) 
 }
